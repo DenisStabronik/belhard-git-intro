a7c81c76b730d3a4784b4b859bd5578542b9635b
DenisStabronik <denisstabronik11@gmail.com>
2021-11-17 21:41:31 +0300

    объеденили

--- Имя.py
+++ Имя.py
@@ -4,3 +4,21 @@ print('Меня зовут Петя')
 print('Хорошая погода не правда ли?')
 answer = input('Ответить что то ')
 print(answer)
+if answer == 'да' or answer == 'Да':
+    print('я рад что тебе нравится')
+else:
+    print('почему?')
+answer_2 = input('Ответить что то ')
+print(answer_2)
+if answer == 'да' or answer == 'Да':
+    print('я рад что тебе нравится')
+else:
+    print('почему?')
+answer_2 = input('Ответить что то ')
+print(answer_2)
+if answer == 'да' or answer == 'Да':
+    print('я рад что тебе нравится')
+else:
+    print('почему?')
+answer_2 = input('Ответить что то ')
+print(answer_2)
